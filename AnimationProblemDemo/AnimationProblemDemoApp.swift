33630ccc1902f854b406c788b2113b8886ed3239
Dennis Müller <d3mueller@me.com>
2022-02-20 13:51:22 +0100

    Initial Commit

---
+++ AnimationProblemDemo/AnimationProblemDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationProblemDemoApp.swift
+//  AnimationProblemDemo
+//
+//  Created by Dennis Müller on 20.02.22.
+//
+
+import SwiftUI
+
+@main
+struct AnimationProblemDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
